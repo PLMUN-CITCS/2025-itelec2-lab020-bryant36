745ebb5574d089aff844615493759899b7acb6ef
unknown <manaladbryantray_bsit@plmun.edu.ph>
2025-02-22 16:32:42 -0700

    Submitting Python Week 05 - Laboratory # 20

---
+++ even_odd_checker_functions.py
@@ -0,0 +1,17 @@
+# BRYANT RAY M. MANALAD
+# ITELEC2
+# Laboratory # 20 - Group Activity # 01 - Problem 02: Even or Odd Checker with Input and Logic Functions
+
+def main():
+    try:
+        number = int(input("Enter an integer: "))
+        if number % 2 == 0:
+            result = f"{number} is an Even number."
+        else:
+            result = f"{number} is an Odd number."
+        print(result)
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
+if __name__ == "__main__":
+    main()
